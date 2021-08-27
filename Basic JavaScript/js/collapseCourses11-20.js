84bbccdeb4afaf4dc2027a6623b8f91cd75b38d6
mahmoud swedani <mahmoudkasimswedani@gmail.com>
2021-08-27 21:25:32 +0300

    collapseCourses11-20

---
+++ Basic JavaScript/js/collapseCourses11-20.js
@@ -0,0 +1,82 @@
+// 11
+// Divide One Number by Another with JavaScript
+var quotient = 4 / 2;
+var divide = 15/3;
+// outbot
+// quotient = 2
+// divide = 5
+
+// 12
+// Increment a Number with JavaScript
+
+var myVar = 87;
+
+// Only change code below this line
+myVar++;
+// outbot
+// myVar = 88
+
+// 13
+// Decrement a Number with JavaScript
+var myVar = 11;
+
+// Only change code below this line
+myVar--;
+
+// 14
+// Create Decimal Numbers with JavaScript
+var ourDecimal = 5.7;
+
+// Only change code below this line
+var myDecimal = 6.3;
+
+// 15
+// Multiply Two Decimals with JavaScript
+var product = 2.0 * 2.5;
+
+// 16
+// Divide One Decimal by Another with JavaScript
+var quotient = 4.4 / 2.0; // Change this line
+
+// 17
+// Finding a Remainder in JavaScript
+// Only change code below this line
+
+var remainder;
+remainder = 11 % 3;
+
+// 18
+// Compound Assignment With Augmented Addition
+var a = 3;
+var b = 17;
+var c = 12;
+
+// Only change code below this line
+a += 12;
+b += 9;
+c += 7;
+
+// 19
+// Compound Assignment With Augmented Subtraction
+var a = 11;
+var b = 9;
+var c = 3;
+
+// Only change code below this line
+a -= 6;
+b -= 15;
+c -= 1;
+
+// 20
+// Compound Assignment With Augmented Multiplication
+var a = 5;
+var b = 12;
+var c = 4.6;
+
+// Only change code below this line
+a *= 5;
+b *= 3;
+c *= 10;
+
+
+
