17816994050c69b8410c9b58ffdcaa7fc4bd50ef
mahmoud swedani <mahmoudkasimswedani@gmail.com>
2021-08-28 12:50:03 +0300

    collapseCourses21-30

---
+++ Basic JavaScript/js/collapseCourses21-30.js
@@ -0,0 +1,66 @@
+// 21
+// Compound Assignment With Augmented Division
+var a = 48;
+var b = 108;
+var c = 33;
+
+// Only change code below this line
+a /= 12;
+b /= 4;
+c /= 11;
+
+// 22
+// Declare String Variables
+var myFirstName = "mahmoud";
+var myLastName = "swedani";
+
+// 23
+// Escaping Literal Quotes in Strings
+var myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
+
+// 24
+// Quoting Strings with Single Quotes
+var myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+
+// 25
+// Escape Sequences in Strings
+var myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
+
+// 26
+// Concatenating Strings with Plus Operator
+var myStr = "This is the start. " + "This is the end."; // Change this line
+
+// 27
+// Concatenating Strings with the Plus Equals Operator
+// Only change code below this line
+
+var myStr = "This is the first sentence. ";
+myStr += "This is the second sentence.";
+
+
+// 28
+// Constructing Strings with Variables
+// Only change code below this line
+var myName = "My name is";
+var myStr = "and I am well!" + myName + "end";
+
+
+// 29
+// Appending Variables to Strings
+// Change code below this line
+
+var someAdjective = "mahmoud";
+var myStr = "Learning to code is ";
+myStr += someAdjective;
+
+
+// 30
+// Find the Length of a String
+// Setup
+var lastNameLength = 0;
+var lastName = "Lovelace";
+
+// Only change code below this line
+
+lastNameLength = lastName.length;
