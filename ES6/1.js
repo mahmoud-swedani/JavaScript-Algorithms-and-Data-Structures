5c357345d3d418b7c43bcbb494b55dd8a8cce001
mahmoud swedani <mahmoudkasimswedani@gmail.com>
2021-08-29 21:41:27 +0300

    Explore Differences Between the var, let Keywords

---
+++ ES6/1.js
@@ -0,0 +1,10 @@
+// Explore Differences Between the var and let Keywords
+let catName;
+let quote;
+function catTalk() {
+  "use strict";
+  catName = "Oliver";
+  quote = catName + " says Meow!";
+  return quote
+}
+catTalk();
