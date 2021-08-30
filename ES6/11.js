3d170d354d58d84e20d1283ca24d41231946d482
mahmoud swedani <mahmoudkasimswedani@gmail.com>
2021-08-30 19:38:05 +0300

    Destructuring Assig to Extract Values from Objects

---
+++ ES6/11.js
@@ -0,0 +1,16 @@
+// Use Destructuring Assignment to Extract Values from Objects
+const HIGH_TEMPERATURES = {
+    yesterday: 75,
+    today: 77,
+    tomorrow: 80
+  };
+  
+  // Only change code below this line
+  
+  const {today,tomorrow} = HIGH_TEMPERATURES;
+  
+  
+  
+  console.log(today)
+  // Only change code above this line
+  
