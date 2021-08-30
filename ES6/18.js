bf8c3cc95c489de6b7ebd9401210fdaa591a5b5e
mahmoud swedani <mahmoudkasimswedani@gmail.com>
2021-08-30 21:24:18 +0300

    18 ch

---
+++ ES6/18.js
@@ -0,0 +1,13 @@
+// Write Concise Object Literal Declarations Using Object Property Shorthand
+const createPerson = (name, age, gender) => {
+  // Only change code below this line
+  return {
+    name,
+     age,
+    gender
+  };
+  // Only change code above this line
+};
+
+
+console.log(createPerson("Zodiac Hasbro", 56, "male"))
