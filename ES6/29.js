c60260c874b6628706f917d9405c61a25314caa9
mahmoud swedani <mahmoudkasimswedani@gmail.com>
2021-08-31 04:36:43 +0300

    Complete a Promise with resolve and reject

---
+++ ES6/29.js
@@ -0,0 +1,12 @@
+// Complete a Promise with resolve and reject
+const makeServerRequest = new Promise((resolve, reject) => {
+    // responseFromServer represents a response from a server
+    let responseFromServer;
+      
+    if(responseFromServer) {
+      resolve("We got the data");
+    } else {  
+      reject("Data not received");
+    }
+  });
+  console.log(makeServerRequest)
