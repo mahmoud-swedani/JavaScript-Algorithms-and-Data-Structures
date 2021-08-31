03b2f66d38fecc334a1a368f3aed4295ac8bbe06
mahmoud swedani <mahmoudkasimswedani@gmail.com>
2021-09-01 00:12:38 +0300

    Find More Than the First Match

---
+++ Regular Expressions/6.js
@@ -0,0 +1,5 @@
+// Find More Than the First Match
+let twinkleStar = "Twinkle, twinkle, little star";
+let starRegex = /Twinkle/gi; // Change this line
+let result = twinkleStar.match(starRegex); // Change this line
+console.log(result);
