6adadf914ecd3bf5eebd892cdfe2910b2768379d
mahmoud swedani <mahmoudkasimswedani@gmail.com>
2021-08-31 04:23:31 +0300

    Reuse JavaScript Code Using import

---
+++ ES6/24.js
@@ -0,0 +1,6 @@
+// Reuse JavaScript Code Using import
+import {uppercaseString, lowercaseString} from './string_functions.js'
+// Only change code above this line
+
+uppercaseString("hello");
+lowercaseString("WORLD!");
