c5011e6ad3e025e71f94349c7b4d06fd9938a01e
mahmoud swedani <mahmoudkasimswedani@gmail.com>
2021-09-01 05:02:18 +0300

    Match Characters that Occur Zero or More Times

---
+++ Regular Expressions/13.js
@@ -0,0 +1,7 @@
+//  Match Characters that Occur Zero or More Times
+// Only change code below this line
+let chewieRegex = /Aa*/; // Change this line
+// Only change code above this line
+
+let result = chewieQuote.match(chewieRegex);
+console.log(result);
