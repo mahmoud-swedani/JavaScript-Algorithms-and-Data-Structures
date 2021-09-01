55d90cc7a942036161c0ddd656dcfab8a7c2723a
mahmoud swedani <mahmoudkasimswedani@gmail.com>
2021-09-01 04:54:35 +0300

    Match Numbers and Letters of the Alphabet

---
+++ Regular Expressions/10.js
@@ -0,0 +1,5 @@
+//  Match Numbers and Letters of the Alphabet
+let quoteSample = "Blueberry 3.141592653s are delicious.";
+let myRegex = /[h-s2-6]/ig; // Change this line
+let result = quoteSample.match(myRegex); // Change this line
+console.log(result);
