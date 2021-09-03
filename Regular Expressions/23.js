d122400e7ea2ce201da983b42154846ef5577595
mahmoud swedani <mahmoudkasimswedani@gmail.com>
2021-09-04 01:49:39 +0300

    Match Whitespace

---
+++ Regular Expressions/23.js
@@ -0,0 +1,5 @@
+//  Match Whitespace
+let sample = "Whitespace is important in separating words";
+let countWhiteSpace = /\s/g; // Change this line
+let result = sample.match(countWhiteSpace);
+console.log(result);
