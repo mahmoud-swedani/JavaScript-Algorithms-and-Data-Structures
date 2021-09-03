fdc5325e2d2f7a4c1660d344995231306232b19a
mahmoud swedani <mahmoudkasimswedani@gmail.com>
2021-09-03 23:15:11 +0300

    Match All NON-Numbers

---
+++ Regular Expressions/21.js
@@ -0,0 +1,5 @@
+//  Match All NON-Numbers
+let movieName = "2001: A Space Odyssey";
+let noNumRegex = /\D/g; // Change this line
+let result = movieName.match(noNumRegex).length;
+console.log(result);
