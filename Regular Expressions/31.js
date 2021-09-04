06fd4e2d65b2a14201ce03977e1b426bda8790b4
mahmoud swedani <mahmoudkasimswedani@gmail.com>
2021-09-04 17:34:31 +0300

    Reuse Patterns Using Capture Groups

---
+++ Regular Expressions/31.js
@@ -0,0 +1,6 @@
+// Reuse Patterns Using Capture Groups
+
+let repeatNum = "42 42 42";
+let reRegex = /^(\d+)\s\1\s\1$/; // Change this line
+let result = reRegex.test(repeatNum);
+console.log(result);
