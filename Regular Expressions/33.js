9b157c8b2a18a22c71d06946e1a888f6a9580d0d
mahmoud swedani <mahmoudkasimswedani@gmail.com>
2021-09-04 20:47:00 +0300

    Remove Whitespace from Start and End

---
+++ Regular Expressions/33.js
@@ -0,0 +1,5 @@
+// Remove Whitespace from Start and End
+let hello = "   Hello, World!  ";
+let wsRegex = /^\s+|\s+$/g; // Change this line
+let result = hello.replace(wsRegex, ""); // Change this line
+console.log(result);
