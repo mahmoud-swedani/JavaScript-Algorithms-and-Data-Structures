cfa44b00fc926420299fc784f2c1b80f7dab8146
mahmoud swedani <mahmoudkasimswedani@gmail.com>
2021-09-06 19:48:32 +0300

    Boo who

---
+++ Basic Algorithm Scripting/10.js
@@ -0,0 +1,11 @@
+// Boo who
+function booWho(bool) {
+    if(bool === true || bool ===false){
+      return true;
+    }else {
+      
+    return false;
+    }
+  }
+  
+  console.log(booWho(false));
