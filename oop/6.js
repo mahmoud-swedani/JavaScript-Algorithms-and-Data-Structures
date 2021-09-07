ae4ae21d6860f8c4a15d56541ea7c6a8434e75c4
mahmoud swedani <mahmoudkasimswedani@gmail.com>
2021-09-07 05:11:26 +0300

    Use a Constructor to Create Objects

---
+++ oop/6.js
@@ -0,0 +1,10 @@
+//Use a Constructor to Create Objects
+ function Dog() {
+  this.name = "Rupert";
+  this.color = "brown";
+  this.numLegs = 4;
+}
+// Only change code below this line
+let hound = new Dog();
+
+console.log(hound);
