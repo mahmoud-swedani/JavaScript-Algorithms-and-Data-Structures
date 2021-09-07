1169f6960a2a5599779b262eeb630a50b6cef52e
mahmoud swedani <mahmoudkasimswedani@gmail.com>
2021-09-07 05:11:39 +0300

    Extend Constructors to Receive Arguments

---
+++ oop/7.js
@@ -0,0 +1,8 @@
+// Extend Constructors to Receive Arguments
+function Dog(name, color) {
+    this.name = name;
+    this.color = color;
+    this.numLegs = 4;
+  }
+  let terrier = new Dog("ms","white")
+  console.log(terrier)
