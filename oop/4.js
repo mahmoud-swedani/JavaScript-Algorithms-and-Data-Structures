707d357d2d47301d40a0a1a78e04e27a017ba6dc
mahmoud swedani <mahmoudkasimswedani@gmail.com>
2021-09-07 05:10:59 +0300

    Make Code More Reusable with the this Keyword

---
+++ oop/4.js
@@ -0,0 +1,8 @@
+// Make Code More Reusable with the this Keyword
+let dog = {
+    name: "Spot",
+    numLegs: 4,
+    sayLegs: function() {return "This dog has " + this.numLegs + " legs.";}
+  };
+  
+  dog.sayLegs();
