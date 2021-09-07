fd65f3e49aa64a19b4cb4734d52320b3d7abbfa2
mahmoud swedani <mahmoudkasimswedani@gmail.com>
2021-09-07 05:16:14 +0300

    Verify an Object's Constructor with instanceof

--- oop/8.js
+++ oop/8.js
@@ -1,1 +1,9 @@
-// 
+// Verify an Object's Constructor with instanceof
+
+function House(numBedrooms) {
+    this.numBedrooms = numBedrooms;
+  }
+  let myHouse = new House(5)
+  console.log(myHouse instanceof House);
+  // Only change code below this line
+  
