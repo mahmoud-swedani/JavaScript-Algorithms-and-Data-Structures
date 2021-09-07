1696cb9f6418fc85c9bdc8638eff5f68e4c6ac48
mahmoud swedani <mahmoudkasimswedani@gmail.com>
2021-09-07 05:10:43 +0300

    Extend Constructors to Receive Arguments

---
+++ oop/3.js
@@ -0,0 +1,9 @@
+// Create a Method on an Object
+let dog = {
+    name: "Spot",
+    numLegs: 4,
+    sayLegs: function() {return "This dog has " + dog.numLegs + " legs.";}
+  
+  };
+  
+  console.log(dog.sayLegs());
