f4119a70a0364748af6750a9190699f1bc458eed
mahmoud swedani <mahmoudkasimswedani@gmail.com>
2021-09-08 03:22:01 +0300

    Understand the Prototype Chain

---
+++ oop/16.js
@@ -0,0 +1,11 @@
+// Understand the Prototype Chain
+function Dog(name) {
+    this.name = name;
+  }
+  
+  let beagle = new Dog("Snoopy");
+  
+  Dog.prototype.isPrototypeOf(beagle);  // yields true
+  
+  // Fix the code below so that it evaluates to true
+  console.log(Object.prototype.isPrototypeOf(Dog.prototype));
