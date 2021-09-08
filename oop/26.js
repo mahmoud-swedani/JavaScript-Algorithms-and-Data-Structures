d112383f3385f809b62b579483cc217c62371c1c
mahmoud swedani <mahmoudkasimswedani@gmail.com>
2021-09-08 03:28:15 +0300

    Use an IIFE to Create a Module

---
+++ oop/26.js
@@ -0,0 +1,16 @@
+// Use an IIFE to Create a Module
+
+
+let funModule  = (function () {
+    return {
+      isCuteMixin : function(obj) {
+    obj.isCute = function() {
+      return true;
+    };
+  },
+     singMixin: function(obj) {
+    obj.sing = function() {
+      console.log("Singing to an awesome tune");
+    };
+  }
+    }})();
