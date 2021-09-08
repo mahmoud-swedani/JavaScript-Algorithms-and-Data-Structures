6cdc2753527b9efccd363ccea55d185bb9b4f182
mahmoud swedani <mahmoudkasimswedani@gmail.com>
2021-09-08 03:20:41 +0300

    Change the Prototype to a New Object

---
+++ oop/13.js
@@ -0,0 +1,17 @@
+// Change the Prototype to a New Object
+function Dog(name) {
+  this.name = name;
+}
+
+Dog.prototype = {
+  // Only change code below this line
+    numLegs: 2, 
+    eat: function() {
+          console.log("nom nom nom");
+        },
+    describe: function() {
+          console.log("My name is " + this.name);
+        }
+};
+
+console.log(new Dog("mahmoud"))
