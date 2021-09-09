0760ce3d196a9bd76acd4d035a92db9cfd7262f4
mahmoud swedani <mahmoudkasimswedani@gmail.com>
2021-09-09 23:59:04 +0300

    Split a String into an Array

---
+++ Functional Programming/19.js
@@ -0,0 +1,9 @@
+// Split a String into an Array Using the split Method
+function splitify(str) {
+    // Only change code below this line
+    let s = str.split(/[-,.\s]/);
+    return s
+  
+    // Only change code above this line
+  }
+  console.log(splitify("Hello World,I-am code"));
