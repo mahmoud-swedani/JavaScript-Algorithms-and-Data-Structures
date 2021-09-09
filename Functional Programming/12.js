4acf25b6510c52959dff8821bf848efdcc5d9fef
mahmoud swedani <mahmoudkasimswedani@gmail.com>
2021-09-09 23:18:23 +0300

    Remove Elements from an Array Using slice

---
+++ Functional Programming/12.js
@@ -0,0 +1,9 @@
+// Remove Elements from an Array Using slice Instead of splice
+function nonMutatingSplice(cities) {
+    // Only change code below this line
+    return cities.slice(0,3);
+  
+    // Only change code above this line
+  }
+  var inputCities = ["Chicago", "Delhi", "Islamabad", "London", "Berlin"];
+  console.log(nonMutatingSplice(inputCities));
