55704e50d560e60dbc1daa458c6d3c46f9c0092e
mahmoud swedani <mahmoudkasimswedani@gmail.com>
2021-09-10 01:16:30 +0300

    Wherefore art thou

---
+++ Intermediate Algorithm Scripting/4.js
@@ -0,0 +1,18 @@
+// Wherefore art thou
+function whatIsInAName(collection, source) {
+
+    var srcKeys = Object.keys(source);
+  
+    return collection.filter(function(obj) {
+      return srcKeys.every(function(key) {
+        return obj.hasOwnProperty(key) && obj[key] === source[key];
+      });
+    });
+  }
+  
+    // Only change code above this line
+  
+  
+  
+  
+  console.log(whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" }))
