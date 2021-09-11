1bec28c6bf04be26c87eb9f88953071a5eaae918
mahmoud swedani <mahmoudkasimswedani@gmail.com>
2021-09-11 05:31:33 +0300

    Arguments Optional

---
+++ Intermediate Algorithm Scripting/19.js
@@ -0,0 +1,35 @@
+// Arguments Optional
+function addTogether() {
+    const [first, second] = arguments;
+    // First argument is not a number
+    if (typeof(first) !== "number") {
+      return undefined;
+    }
+    // First argument is a number
+    //  and second argument is not defined
+    else if (second === undefined) {
+      function addSecond(second) {
+        // New argument is not a number
+        if (typeof(second) !== "number") {
+          return undefined;
+        }
+        // New argument is a number
+        else {
+          return first + second;
+        }
+      }
+      // Note: returning a *function*
+      return addSecond;
+    }
+    // First argument is a number
+    //  and second argument is not a number
+    else if (typeof(second) !== "number") {
+      return undefined;
+    }
+    // First argument is a number
+    //  and second argument is a number
+    else {
+      return first + second;
+    }
+  }
+  addTogether(2,3);
