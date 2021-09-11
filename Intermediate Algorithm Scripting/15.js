11ac59c547387dfe6630d15a0f2df20272fd99e0
mahmoud swedani <mahmoudkasimswedani@gmail.com>
2021-09-11 05:20:10 +0300

    Drop it

---
+++ Intermediate Algorithm Scripting/15.js
@@ -0,0 +1,13 @@
+// Drop it
+
+function dropElements(arr, func) {
+    while (arr.length > 0 && !func(arr[0])) {
+      arr.shift();
+    }
+    return arr;
+  }
+  
+  // test here
+  dropElements([1, 2, 3, 4], function(n) {
+    return n >= 3;
+  });
